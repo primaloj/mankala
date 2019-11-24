ce470df483d1b723681858739f18de353cdf632c
primaloj <58123173+primaloj@users.noreply.github.com>
2019-11-23 16:19:56 -0800

    phoo5

--- app/src/main/java/com/primaloj/mankala/SettingsActivity.kt
+++ app/src/main/java/com/primaloj/mankala/SettingsActivity.kt
@@ -2,11 +2,19 @@ package com.primaloj.mankala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        findViewById<TextView>(R.id.check_sound_label).setOnClickListener { toggleSound() }
+    }
+
+    fun toggleSound() {
+        val checkSound = findViewById<CheckBox>(R.id.check_sound)
+        checkSound.isChecked = !checkSound.isChecked
     }
 }
