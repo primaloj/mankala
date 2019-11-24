16341e72daf36ae92ac6883ffc5f1220594258d7
primaloj <58123173+primaloj@users.noreply.github.com>
2019-11-23 16:08:09 -0800

    phoo4

--- app/src/main/java/com/primaloj/mankala/HomeActivity.kt
+++ app/src/main/java/com/primaloj/mankala/HomeActivity.kt
@@ -1,12 +1,21 @@
 package com.primaloj.mankala
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        findViewById<Button>(R.id.button_settings).setOnClickListener { openSettings() }
+    }
+
+    fun openSettings() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
+
