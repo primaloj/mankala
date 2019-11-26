013dee2d0c4f897e9aa70dcb028f6ebb04893d34
primaloj <58123173+primaloj@users.noreply.github.com>
2019-11-25 20:00:55 -0800

    plop1

--- app/src/main/java/com/primaloj/mankala/SelectNameActivity.kt
+++ app/src/main/java/com/primaloj/mankala/SelectNameActivity.kt
@@ -1,6 +1,7 @@
 package com.primaloj.mankala
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectNameActivity : AppCompatActivity() {
@@ -8,6 +9,14 @@ class SelectNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectname)
+        findViewById<ImageButton>(R.id.button_close).setOnClickListener { close() }
+    }
+
+
+    fun close() {
+        finish()
     }
 }
+
+
 
