f4bc8dcb82cdbcf82ded64206ca007a4d64ee693
primaloj <58123173+primaloj@users.noreply.github.com>
2019-12-05 18:26:22 -0800

    newera1

--- app/src/main/java/com/primaloj/mankala/GameActivity.kt
+++ app/src/main/java/com/primaloj/mankala/GameActivity.kt
@@ -1,13 +1,23 @@
 package com.primaloj.mankala
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+
 
 class GameActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         val p1_name = "p1_name"
         val p2_name = "p2_name"
     }
@@ -17,6 +27,22 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
         findViewById<TextView>(R.id.p1_name).text = intent.getStringExtra(p1_name)
         findViewById<TextView>(R.id.p2_name).text = intent.getStringExtra(p2_name)
+        createPits()
+    }
+
+    fun createPits() {
+        val container = findViewById<LinearLayout>(R.id.pits_container)
+        for (i in 1..8) {
+            val view = View(this)
+            val layoutParams = LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.MATCH_PARENT,
+                1F
+            )
+            val rnd = Random()
+            val color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+            view.setBackgroundColor(color)
+            container.addView(view,layoutParams)
+        }
     }
 
 }
