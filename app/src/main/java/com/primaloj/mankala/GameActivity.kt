a19f6336d3f43de9c269f534935a7596ba849731
primaloj <58123173+primaloj@users.noreply.github.com>
2019-12-08 16:15:01 -0800

    ok2

--- app/src/main/java/com/primaloj/mankala/GameActivity.kt
+++ app/src/main/java/com/primaloj/mankala/GameActivity.kt
@@ -1,18 +1,11 @@
 package com.primaloj.mankala
 
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import java.util.*
 
 
@@ -33,24 +26,18 @@ class GameActivity : AppCompatActivity() {
     fun createPits() {
         val container = findViewById<LinearLayout>(R.id.pits_container)
         for (i in 1..8) {
+            var view: View
             if (i == 1 || i == 8) {
-                val view = View(this)
-                val layoutParams = LinearLayout.LayoutParams(
-                    0, LinearLayout.LayoutParams.MATCH_PARENT,
-                    1F
-                )
-                val rnd = Random()
-                val color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
-                view.setBackgroundColor(color)
-                container.addView(view, layoutParams)
+                view = View(this)
             } else {
-                val doublePit = DoublePit(this)
-                val layoutParams = LinearLayout.LayoutParams(
-                    0, LinearLayout.LayoutParams.MATCH_PARENT,
-                    1F
-                )
-                container.addView(doublePit, layoutParams)
+                view = DoublePit(this)
             }
+            val layoutParams = LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.MATCH_PARENT,
+                1F
+            )
+            container.addView(view, layoutParams)
+
         }
     }
 
