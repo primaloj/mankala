d18e4bff02a10ebbddfdf65886b84babadd154a0
primaloj <58123173+primaloj@users.noreply.github.com>
2020-01-05 10:52:36 -0800

    c

--- app/src/main/java/com/primaloj/mankala/HomeActivity.kt
+++ app/src/main/java/com/primaloj/mankala/HomeActivity.kt
@@ -2,7 +2,12 @@ package com.primaloj.mankala
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.BounceInterpolator
+import android.view.animation.OvershootInterpolator
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,20 +20,55 @@ class HomeActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.activity_home)
-        findViewById<ImageButton>(R.id.button_settings).setOnClickListener { openSettings() }
-        findViewById<ImageButton>(R.id.button_play).setOnClickListener { openSelectName() }
+        findViewById<ImageButton>(R.id.button_settings).setOnTouchListener { v, event ->
+            onSettingsButtonTouched(v, event)
+        }
+        findViewById<ImageButton>(R.id.button_play).setOnTouchListener { v, event ->
+            onPlayButtonTouched(v, event)
+        }
     }
 
-    fun openSettings() {
+    private fun openSettings() {
         val intent = Intent(this, SettingsActivity::class.java)
         startActivity(intent)
     }
 
-    fun openSelectName() {
+    private fun openSelectName() {
         val intent = Intent(this, SelectNameActivity::class.java)
         startActivity(intent)
     }
-}
 
+    private fun onPlayButtonTouched(v: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                v.animate().scaleX(0.9F).scaleY(0.9F).setDuration(40).start()
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                v.animate().scaleX(1.0F).scaleY(1.0F).setDuration(40).start()
+                openSelectName()
+                true
+            }
+        }
+
+        return true
+    }
+
+    private fun onSettingsButtonTouched(v: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                v.animate().scaleX(0.9F).scaleY(0.9F).setDuration(40).start()
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                v.animate().scaleX(1.0F).scaleY(1.0F).setDuration(40).start()
+                openSettings()
+                true
+            }
+        }
+        return true
+
+    }
+}
 
 
