ed262321fcbf881a91a8f659b190d56888b1c30f
primaloj <58123173+primaloj@users.noreply.github.com>
2020-01-04 19:19:59 -0800

    f

--- app/src/main/java/com/primaloj/mankala/HomeActivity.kt
+++ app/src/main/java/com/primaloj/mankala/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.primaloj.mankala
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +10,10 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_home)
         findViewById<ImageButton>(R.id.button_settings).setOnClickListener { openSettings() }
         findViewById<ImageButton>(R.id.button_play).setOnClickListener { openSelectName() }
