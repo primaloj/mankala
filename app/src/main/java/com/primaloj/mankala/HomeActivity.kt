21c01337439ecb5fdcd6ae516a90b0ce4c2af128
primaloj <58123173+primaloj@users.noreply.github.com>
2020-01-04 19:15:00 -0800

    z

--- app/src/main/java/com/primaloj/mankala/HomeActivity.kt
+++ app/src/main/java/com/primaloj/mankala/HomeActivity.kt
@@ -1,17 +1,17 @@
 package com.primaloj.mankala
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        findViewById<Button>(R.id.button_settings).setOnClickListener { openSettings() }
-        findViewById<Button>(R.id.button_play).setOnClickListener { openSelectName() }
+        findViewById<ImageButton>(R.id.button_settings).setOnClickListener { openSettings() }
+        findViewById<ImageButton>(R.id.button_play).setOnClickListener { openSelectName() }
     }
 
     fun openSettings() {
