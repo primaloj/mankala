8baa45bf264a6966365c0b10690cb7b895630c05
primaloj <58123173+primaloj@users.noreply.github.com>
2020-01-12 15:07:38 -0800

    refactor doublepit

--- app/src/main/java/com/primaloj/mankala/DoublePit.kt
+++ app/src/main/java/com/primaloj/mankala/DoublePit.kt
@@ -7,51 +7,55 @@ import android.widget.LinearLayout
 class DoublePit(context: Context?) : LinearLayout(context) {
 
     lateinit var onPitSelected: (pit: Pit) -> Unit
+    private var p1pit: Pit
+    private var p2pit: Pit
+
 
     init {
         LayoutInflater.from(context).inflate(R.layout.double_pit, this, true)
-        findViewById<Pit>(R.id.p1_pit).setOnClickListener {
+        p1pit = findViewById(R.id.p1_pit)
+        p2pit = findViewById(R.id.p2_pit)
+
+        p1pit.setOnClickListener {
             onPitSelected.invoke(it as Pit)
         }
-        findViewById<Pit>(R.id.p2_pit).setOnClickListener {
+
+        p2pit.setOnClickListener {
             onPitSelected.invoke(it as Pit)
         }
     }
 
+    @Suppress("unused")
     fun setBorderColors(p1Color: Int, p2Color: Int) {
-        findViewById<Pit>(R.id.p2_pit).setBorderColor(p2Color)
-        findViewById<Pit>(R.id.p1_pit).setBorderColor(p1Color)
-        findViewById<Pit>(R.id.p1_pit).setPlayer(1)
-        findViewById<Pit>(R.id.p2_pit).setPlayer(2)
+        p2pit.setBorderColor(p2Color)
+        p1pit.setBorderColor(p1Color)
+        p1pit.setPlayer(1)
+        p2pit.setPlayer(2)
     }
 
-    fun addOneToPit(pitIndex: Int) {
-        if (pitIndex == 1) {
-            findViewById<Pit>(R.id.p1_pit).increment()
-        } else {
-            findViewById<Pit>(R.id.p2_pit).increment()
-        }
+    fun addOneToPit(index: Int) {
+        getPitForPlayer(index).increment()
     }
 
-    fun reset(pitIndex: Int) {
-        if (pitIndex == 1) {
-            findViewById<Pit>(R.id.p1_pit).reset()
-        } else {
-            findViewById<Pit>(R.id.p2_pit).reset()
-        }
+    fun reset(index: Int) {
+        getPitForPlayer(index).reset()
     }
 
+    @Suppress("unused")
     fun setInitialValues(value: Int) {
-        findViewById<Pit>(R.id.p1_pit).setValue(value)
-        findViewById<Pit>(R.id.p2_pit).setValue(value)
+        p1pit.setValue(value)
+        p2pit.setValue(value)
     }
 
-    fun getValueForPlayer(player: Int): Int {
-        if (player == 1) {
-            return findViewById<Pit>(R.id.p1_pit).getValue()
-        } else {
-            return findViewById<Pit>(R.id.p2_pit).getValue()
+    fun getValueForPlayer(index: Int): Int {
+        return getPitForPlayer(index).getValue()
+    }
 
+    private fun getPitForPlayer(index: Int): Pit {
+        return if (index == 1) {
+            p1pit
+        } else {
+            p2pit
         }
     }
 }
