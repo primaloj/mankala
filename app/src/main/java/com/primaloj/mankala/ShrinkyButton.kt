ed8e0b615ffa6e6ee2feff00b1e3e7a7d934a953
primaloj <58123173+primaloj@users.noreply.github.com>
2020-01-12 14:40:12 -0800

    Update ShrinkyButton.kt

--- app/src/main/java/com/primaloj/mankala/ShrinkyButton.kt
+++ app/src/main/java/com/primaloj/mankala/ShrinkyButton.kt
@@ -1,9 +1,11 @@
 package com.primaloj.mankala
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.ImageButton
+
 
 class ShrinkyButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -11,15 +13,35 @@ class ShrinkyButton @JvmOverloads constructor(
 
     lateinit var onClick: () -> Unit
 
+    private lateinit var rect: Rect
+    private var touchInside: Boolean = false
+
     init {
         setOnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     v.animate().scaleX(0.9F).scaleY(0.9F).setDuration(40).start()
+                    rect =
+                        Rect(v.left, v.top, v.right, v.bottom)
+                    touchInside = true
                 }
                 MotionEvent.ACTION_UP -> {
                     v.animate().scaleX(1.0F).scaleY(1.0F).setDuration(40).start()
-                    onClick.invoke()
+                    if (touchInside) {
+                        onClick.invoke()
+                        touchInside = false
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    if (!rect.contains(
+                            v.left + event.x.toInt(),
+                            v.top + event.y.toInt()
+                        )
+                    ) {
+                        // User moved outside bounds
+                        touchInside = false
+                        v.animate().scaleX(1.0F).scaleY(1.0F).setDuration(40).start()
+                    }
                 }
             }
 
