787f4804820d517aff59372d2abb68aeac63ff8b
primaloj <58123173+primaloj@users.noreply.github.com>
2020-02-23 17:13:13 -0800

    iconyafe

--- app/src/main/java/com/primaloj/mankala/GameSettings.kt
+++ app/src/main/java/com/primaloj/mankala/GameSettings.kt
@@ -4,6 +4,7 @@ import android.content.Context
 
 object GameSettings {
     private val keySoundEnabled = "sound_enabled"
+    private val keyMarbleCount = "marble_count"
 
     private fun getSharedPrefs(context: Context) =
         context.getSharedPreferences("mankala", Context.MODE_PRIVATE)
@@ -16,5 +17,13 @@ object GameSettings {
 
     fun isSoundEnabled(context: Context): Boolean {
         return getSharedPrefs(context).getBoolean(keySoundEnabled, true)
+    }
+    fun setMarbleCount(context: Context, count: Int) {
+        val editor = getSharedPrefs(context).edit()
+        editor.putInt(keyMarbleCount, count)
+        editor.apply()
+    }
+    fun getMarbleCount(context: Context): Int {
+        return getSharedPrefs(context).getInt(keyMarbleCount, 4)
     }
 }
